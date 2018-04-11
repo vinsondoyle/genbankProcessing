4b233147539555a18c050469cbc23a2403ee967e
vinson doyle <sonofvin@gmail.com>
2018-04-11 12:41:34 -0500

    Changed to have reformatFasta.py put underscores rather than periods in fasta description line due to problems with jModelTest on linux ubuntu not reading those files.

--- reformatFasta.py
+++ reformatFasta.py
@@ -10,9 +10,10 @@ with open(sys.argv[1], 'r') as f:
     for line in f:
         if line.startswith(">"):
             accession = line.split()[0]
+            accession = accession.split(".")[0]
             genus = line.split()[1]
             species = line.split()[2]
-            newFile.write(accession + "." + genus + "." + species + "\n")
+            newFile.write(accession + "_" + genus + "_" + species + "\n")
         else:
             newFile.write(line)
 
